96990189cdb10610d80acf01255bd18260dcfb44
Harinand <harinandsp@gmail.com>
2026-06-17 21:00:01 +0530

    Add files via upload

---
+++ d4/3.js
@@ -0,0 +1,77 @@
+// //PRIMITIVE DATA TYPE
+// //1. number data type - numeric value with or without decimal point
+// var num = 10;
+// var nums = 10.5;
+// console.log(num,nums,typeof (num),typeof (nums));
+
+// //2. string data type - combination of characters enclosed in single or double or backticks
+// var name = "mithu";
+// var nm1 = "krmu";
+// var nm2 = `sohna
+// gurugram
+//            HR`;
+// console.log(name,nm1,nm2, typeof (name),typeof (nm1),typeof (nm2));
+
+// //3. boolean data type - true or false
+// var bl = false;
+// var bl1 = true;
+// console.log(bl,bl1, typeof (bl),typeof (bl1));
+
+// //4. null data type - empty value
+// var n1 = null;
+// console.log(n1, typeof (n1));
+
+// //5. undefined data type - variable is declared but not assigned any value
+// var n2;
+// console.log(n2, typeof (n2));
+
+
+//<-------------------------None Primitive --------------------->
+// let arr = [1,2,3];
+// console.log(arr,typeof (arr));
+
+// let obj = {a:1};
+// console.log(obj,typeof(obj));
+
+// function hello(){
+//     console.log("hello world");
+// }
+
+// function yello(){
+//     return "hello world";
+// }
+// console.log(hello,typeof (hello));
+
+
+
+
+// 3. MATHEMATICAL OPERATOR/ARITHEMATIC OPERATORS [+,-,*,/,%,**]
+// var a = 10;
+// var b = 3;
+// console.log(Math.floor(a/b));
+// console.log(Math.ceil(a/b));
+
+// console.log(Math.random()*1000);
+
+
+
+// //4. relational operator <,>, <=,>=
+// var a = 3,b=3;
+
+// console.log(a>b); //f
+// console.log(a<b); //f
+// console.log(a>=b); //t
+// console.log(a<=b); //t
+
+
+// //5. Comparison operator ==,===,!=,!==
+//  //== comparison only data/value
+
+
+//  //=== comparison data/value and data type
+
+//  console.log(3==3); //t
+//  console.log(1+2==="3");//f
+//  console.log(.1+.2==.3); //f
+//  console.log(.1+.3===.4); //t
+//  console.log("hello"=="Hello"); //f
---
+++ d4/6.js
@@ -0,0 +1,21 @@
+// //Logical Operators and &&, or ||, not !
+
+// //Not Operators
+
+// console.log(!"1");//false
+// console.log(!!0); //false
+// console.log(!1); //false
+// console.log(!undefined); //true
+
+
+
+// // && operator - all the values should be truthy then only it will return the last value otherwise it will return the first falsy value
+// console.log("ram" && "shyam" && 0.5&&""&&"mukesh");
+
+//  // || operator - if any one value is truthy then it will return the first truthy value otherwise it will return the last falsy value
+//  console.log("ram" || "shyam" || 0.5||""||"mukesh");
+
+// //PRIORITY OF LOGICAL OPERATORS
+// 1) NOT > AND > OR
+
+
