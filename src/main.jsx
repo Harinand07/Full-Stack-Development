435de47dca70a20f81f656ca074e8978af1de757
Harinand <harinadsp@gmail.com>
2026-06-25 11:50:49 +0530

    Initial commit: Pokemon Pokédex Project with React, Vite, and dark mode

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import AuthProvider from './Context/AuthProvider.jsx'
+import ThemeProvider from './Context/ThemeProvider.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </ThemeProvider>
+  </StrictMode>,
+)
